0ad701372533a5819a4e3d64204456f6c7a8aa4d
Luligu <target.epithet-0h@icloud.com>
2026-06-07 12:45:16 +0100

    Add warning on tests for AI

--- src/module.auto.test.ts
+++ src/module.auto.test.ts
@@ -1,6 +1,10 @@
 // src\module.matter.test.ts
 
-// Warning: the tests in this unit are supposed to run sequentially cause they depend on the Matterbridge/Matter state.
+/**
+ * WARNING!!!
+ * The tests in this unit are supposed to run sequentially cause they depend on the Matterbridge/Matter state.
+ * Is not possible for timing resonons to create and destroy a Matter node each test to keep isolation.
+ */
 
 /* eslint-disable no-console */
 
--- src/module.test.ts
+++ src/module.test.ts
@@ -1,6 +1,10 @@
 // src\module.test.ts
 
-// Warning: the tests in this unit are supposed to run sequentially cause they depend on the Matterbridge/Matter state.
+/**
+ * WARNING!!!
+ * The tests in this unit are supposed to run sequentially cause they depend on the Matterbridge/Matter state.
+ * Is not possible for timing resonons to create and destroy a Matter node each test to keep isolation.
+ */
 
 /* eslint-disable no-console */
 
